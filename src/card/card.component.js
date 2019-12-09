d22b4276eb20f6475f2cd1b81ed3be22225f24c8
Ba-Zen <bazenatlas@gmail.com>
2019-12-09 16:22:22 -0500

    adds toggle question component to handle logic for revealing answers

--- src/card/card.component.js
+++ src/card/card.component.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import './card.css';
 
+import ToggleQuestions from '../toggle-questions/toggle-questions.component';
 import Question_Data from '../assets/data/questions.data';
 
 class Card extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      showAnswer: false
-    };
-  }
-
   render() {
-    console.log(Question_Data);
     return (
       <div className='card-container'>
         {Question_Data.frontend.questions.map(q => (
           <div key={q.id} className='card'>
-            <h2>{q.question}</h2>
-            <p>{q.answer}</p>
+            <ToggleQuestions question={q.question} answer={q.answer} />
           </div>
         ))}
       </div>
---
+++ src/toggle-questions/toggle-questions.component.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class ToggleQuestions extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isHidden: true
+    };
+  }
+
+  handleToggle = e => {
+    e.preventDefault();
+    this.setState({
+      isHidden: !this.state.isHidden
+    });
+  };
+
+  render() {
+    const { question, answer } = this.props;
+    const { isHidden } = this.state;
+    return (
+      <div>
+        <h2>{question}</h2>
+        {!isHidden && <p>{answer}</p>}
+        <button onClick={this.handleToggle}>Show Answer</button>
+      </div>
+    );
+  }
+}
+
+export default ToggleQuestions;
